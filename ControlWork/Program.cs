be2079f9d46300610c90a7f51f00bd4581e8d7d7
kurilo <jackpotd3@gmail.com>
2019-08-20 21:50:54 +0300

    changed

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -8,9 +8,65 @@ namespace ControlWork
 {
     class Program
     {
+        public static string MostPopulatedCity(City[] cities)
+        {
+
+            City mostPop = new City();
+            foreach (var city in cities)
+            {
+
+                if (city.Population > mostPop.Population)
+                {
+                    mostPop.Name = city.Name;
+                    mostPop.Population = city.Population;
+                    mostPop.Density = city.Density;
+                }
+            }
+            return $"Most Populated: {mostPop.Name} ({mostPop.Population})";
+        }
+        public static string LoggestName(City[] cities)
+        {
+            City mostPop = new City();
+            foreach (var city in cities)
+            {
+
+                if (city.Name.Length > mostPop.Name.Length)
+                {
+                    mostPop.Name = city.Name;
+                    mostPop.Population = city.Population;
+                    mostPop.Density = city.Density;
+                }
+            }
+            return $":ongest Name: {mostPop.Name} ({mostPop.Name.Length} letters)";
+        }
+        public struct City
+        {
+            public string Name;
+            public int Population;
+            public int Density;
+
+           
+        }
+        //Kharkov=1431000,350;Kiev=2804000,839;Las Vegas=603400,352
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine("Input");
+            string input = Console.ReadLine();
+
+            string[] result = input.Split(';');
+            City[] cities = new City[result.Length];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                cities[i].Name = result[i].Split('=')[0];
+                cities[i].Population = int.Parse(result[i].Split('=' ,',')[1]);
+                cities[i].Density = int.Parse(result[i].Split(',')[1]);
+            }
+
+            Console.WriteLine(MostPopulatedCity(cities));
+
+            
+
         }
     }
 }
